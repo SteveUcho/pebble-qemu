3c6cd6e7ee8427d9b402061cfd6e57add286bdb6
Chunyan Liu <cyliu@suse.com>
2014-07-07 14:34:35 +0800

    qemu: support xen hvm direct kernel boot
    qemu side patch to support xen HVM direct kernel boot:
    if -kernel exists, calls xen_load_linux(), which will read kernel/initrd
    and add a linuxboot.bin or multiboot.bin option rom. The
    linuxboot.bin/multiboot.bin will load kernel/initrd and jump to execute
    kernel directly. It's working when xen uses seabios.
    
    During this work, found the 'kvmvapic' is in option_rom list, it should
    not be there in xen case. Set s->vapic_control = 0 in xen_apic_realize()
    to handle that.
    
    Signed-off-by: Chunyan Liu <cyliu@suse.com>
    Acked-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>

--- include/hw/i386/pc.h
+++ include/hw/i386/pc.h
@@ -190,6 +190,11 @@ void pc_set_legacy_acpi_data_size(void);
 void pc_pci_as_mapping_init(Object *owner, MemoryRegion *system_memory,
                             MemoryRegion *pci_address_space);
 
+FWCfgState *xen_load_linux(const char *kernel_filename,
+                           const char *kernel_cmdline,
+                           const char *initrd_filename,
+                           ram_addr_t below_4g_mem_size,
+                           PcGuestInfo *guest_info);
 FWCfgState *pc_memory_init(MachineState *machine,
                            MemoryRegion *system_memory,
                            ram_addr_t below_4g_mem_size,
