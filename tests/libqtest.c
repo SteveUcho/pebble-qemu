c01aadeebc441b66820f69c54deb30ab5bf3838b
Stefan Hajnoczi <stefanha@redhat.com>
2014-08-27 12:08:52 +0100

    libqtest: launch QEMU with QEMU_AUDIO_DRV=none
    No test case actually uses the audio backend.  Disable audio to prevent
    warnings on hosts with no sound hardware present:
    
      GTESTER check-qtest-aarch64
      sdl: SDL_OpenAudio failed
      sdl: Reason: No available audio device
      sdl: SDL_OpenAudio failed
      sdl: Reason: No available audio device
      audio: Failed to create voice `lm4549.out'
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Reviewed-by: Gerd Hoffmann <kraxel@redhat.com>

--- tests/libqtest.c
+++ tests/libqtest.c
@@ -197,6 +197,7 @@ QTestState *qtest_init(const char *extra_args, int num_serial_ports)
 
     s->qemu_pid = fork();
     if (s->qemu_pid == 0) {
+        setenv("QEMU_AUDIO_DRV", "none", true);
         command = g_strdup_printf("exec %s "
                                   "-qtest unix:%s,nowait "
                                   "-qtest-log %s "
