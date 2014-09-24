5b1ef62ab2220e286bf0c5a88a36a91bd530e9aa
Alexander Graf <agraf@suse.de>
2014-09-24 13:06:57 +0200

    sysbus: Add dynamic sysbus device search
    Sysbus devices can be spawned by C code or dynamically via the command line.
    In the latter case, we need to be able to find the dynamically created devices
    to do things with them.
    
    This patch adds a search helper that makes it easy to look for dynamically
    spawned sysbus devices.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- include/hw/sysbus.h
+++ include/hw/sysbus.h
@@ -61,6 +61,8 @@ struct SysBusDevice {
 /* Macros to compensate for lack of type inheritance in C.  */
 #define FROM_SYSBUS(type, dev) DO_UPCAST(type, busdev, dev)
 
+typedef int FindSysbusDeviceFunc(SysBusDevice *sbdev, void *opaque);
+
 void sysbus_init_mmio(SysBusDevice *dev, MemoryRegion *memory);
 MemoryRegion *sysbus_mmio_get_region(SysBusDevice *dev, int n);
 void sysbus_init_irq(SysBusDevice *dev, qemu_irq *p);
@@ -75,6 +77,9 @@ void sysbus_mmio_map_overlap(SysBusDevice *dev, int n, hwaddr addr,
 void sysbus_add_io(SysBusDevice *dev, hwaddr addr,
                    MemoryRegion *mem);
 MemoryRegion *sysbus_address_space(SysBusDevice *dev);
+
+/* Call func for every dynamically created sysbus device in the system */
+void foreach_dynamic_sysbus_device(FindSysbusDeviceFunc *func, void *opaque);
 
 /* Legacy helper function for creating devices.  */
 DeviceState *sysbus_create_varargs(const char *name,
