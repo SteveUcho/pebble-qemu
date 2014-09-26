6deee8804caab70f82b0dc50cdd803bc9a5fa3df
Peter Crosthwaite <peter.crosthwaite@xilinx.com>
2014-09-25 22:22:04 -0700

    irq: Remove qemu_irq_intercept_out
    No more users left and obsoleted by qdev_intercept_gpio_out.
    
    Reviewed-by: Alexander Graf <agraf@suse.de>
    Signed-off-by: Peter Crosthwaite <peter.crosthwaite@xilinx.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/core/irq.c
+++ hw/core/irq.c
@@ -149,16 +149,6 @@ void qemu_irq_intercept_in(qemu_irq *gpio_in, qemu_irq_handler handler,
     }
 }
 
-void qemu_irq_intercept_out(qemu_irq **gpio_out, qemu_irq_handler handler,
-                            int id, int n)
-{
-    IRQInterceptData *intercept_data = g_malloc0(sizeof(IRQInterceptData));
-    qemu_irq *old_irqs = *gpio_out;
-    intercept_data->id = id;
-    intercept_data->old_irqs = old_irqs;
-    *gpio_out = qemu_allocate_irqs(handler, intercept_data, n);
-}
-
 static const TypeInfo irq_type_info = {
    .name = TYPE_IRQ,
    .parent = TYPE_OBJECT,
