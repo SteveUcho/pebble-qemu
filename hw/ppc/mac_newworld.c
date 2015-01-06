1255d1de8bae75f0de80a7db55d8cc1c0b7bc133
Marcel Apfelbaum <marcel@redhat.com>
2015-01-06 15:29:17 +0200

    hw/ppc/mac_newworld: simplify usb controller creation logic
    Signed-off-by: Marcel Apfelbaum <marcel@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- hw/ppc/mac_newworld.c
+++ hw/ppc/mac_newworld.c
@@ -418,8 +418,7 @@ static void ppc_core99_init(MachineState *machine)
     dev = qdev_create(adb_bus, TYPE_ADB_MOUSE);
     qdev_init_nofail(dev);
 
-    if ((machine_arch == ARCH_MAC99_U3 && defaults_enabled()) ||
-        usb_enabled()) {
+    if (machine->usb) {
         pci_create_simple(pci_bus, -1, "pci-ohci");
         /* U3 needs to use USB for input because Linux doesn't support via-cuda
         on PPC64 */
