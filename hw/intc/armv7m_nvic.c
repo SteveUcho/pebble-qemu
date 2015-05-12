7bef9db56d6cc23f5510e70f3f8286fb48d4f8a4
Fabian Aggeler <aggelerf@ethz.ch>
2015-05-12 11:57:17 +0100

    hw/intc/arm_gic: Make ICCICR/GICC_CTLR banked
    ICCICR/GICC_CTLR is banked in GICv1 implementations with Security
    Extensions or in GICv2 in independent from Security Extensions.
    This makes it possible to enable forwarding of interrupts from
    the CPU interfaces to the connected processors for Group0 and Group1.
    
    We also allow to set additional bits like AckCtl and FIQEn by changing
    the type from bool to uint32. Since the field does not only store the
    enable bit anymore and since we are touching the vmstate, we use the
    opportunity to rename the field to cpu_ctlr.
    
    Signed-off-by: Fabian Aggeler <aggelerf@ethz.ch>
    Signed-off-by: Greg Bellows <greg.bellows@linaro.org>
    Reviewed-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Message-id: 1430502643-25909-9-git-send-email-peter.maydell@linaro.org
    Message-id: 1429113742-8371-9-git-send-email-greg.bellows@linaro.org
    [PMM: rewrote to store state in a single uint32_t rather than
     keeping the NS and S banked variants separate; this considerably
     simplifies the get/set functions]
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/intc/armv7m_nvic.c
+++ hw/intc/armv7m_nvic.c
@@ -574,7 +574,7 @@ static void armv7m_nvic_reset(DeviceState *dev)
      * as enabled by default, with a priority mask which allows
      * all interrupts through, and reset all priorities. 
      */
-    s->gic.cpu_enabled[0] = true;
+    s->gic.cpu_ctlr[0] = GICC_CTLR_EN_GRP0;
     s->gic.priority_mask[0] = 0x100;
     int i;
     for (i=0; i<GIC_INTERNAL; i++) {
