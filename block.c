1317049fcff6f706f8577bce61324d2d7e674e10
John Snow <jsnow@redhat.com>
2015-06-08 16:49:15 -0400

    block: record new size in bdrv_dirty_bitmap_truncate
    ce1ffea8 neglected to update the BdrvDirtyBitmap structure
    itself for internal consistency. It's currently not an issue,
    but for migration and persistence series this will cause headaches.
    
    Signed-off-by: John Snow <jsnow@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block.c
+++ block.c
@@ -3246,6 +3246,7 @@ static void bdrv_dirty_bitmap_truncate(BlockDriverState *bs)
             continue;
         }
         hbitmap_truncate(bitmap->bitmap, size);
+        bitmap->size = size;
     }
 }
 
