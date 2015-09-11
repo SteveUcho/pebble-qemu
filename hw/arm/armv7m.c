c6d242da1cd375e5d7ee79023a702306f4e161e6
Markus Armbruster <armbru@redhat.com>
2015-09-11 16:51:43 +0200

    Fix bad error handling after memory_region_init_ram()
    Symptom:
    
        $ qemu-system-x86_64 -m 10000000
        Unexpected error in ram_block_add() at /work/armbru/qemu/exec.c:1456:
        upstream-qemu: cannot set up guest memory 'pc.ram': Cannot allocate memory
        Aborted (core dumped)
    
    Root cause: commit ef701d7 screwed up handling of out-of-memory
    conditions.  Before the commit, we report the error and exit(1), in
    one place, ram_block_add().  The commit lifts the error handling up
    the call chain some, to three places.  Fine.  Except it uses
    &error_abort in these places, changing the behavior from exit(1) to
    abort(), and thus undoing the work of commit 3922825 "exec: Don't
    abort when we can't allocate guest memory".
    
    The three places are:
    
    * memory_region_init_ram()
    
      Commit 4994653 (right after commit ef701d7) lifted the error
      handling further, through memory_region_init_ram(), multiplying the
      incorrect use of &error_abort.  Later on, imitation of existing
      (bad) code may have created more.
    
    * memory_region_init_ram_ptr()
    
      The &error_abort is still there.
    
    * memory_region_init_rom_device()
    
      Doesn't need fixing, because commit 33e0eb5 (soon after commit
      ef701d7) lifted the error handling further, and in the process
      changed it from &error_abort to passing it up the call chain.
      Correct, because the callers are realize() methods.
    
    Fix the error handling after memory_region_init_ram() with a
    Coccinelle semantic patch:
    
        @r@
        expression mr, owner, name, size, err;
        position p;
        @@
                memory_region_init_ram(mr, owner, name, size,
        (
        -                              &error_abort
        +                              &error_fatal
        |
                                       err@p
        )
                                      );
        @script:python@
            p << r.p;
        @@
        print "%s:%s:%s" % (p[0].file, p[0].line, p[0].column)
    
    When the last argument is &error_abort, it gets replaced by
    &error_fatal.  This is the fix.
    
    If the last argument is anything else, its position is reported.  This
    lets us check the fix is complete.  Four positions get reported:
    
    * ram_backend_memory_alloc()
    
      Error is passed up the call chain, ultimately through
      user_creatable_complete().  As far as I can tell, it's callers all
      handle the error sanely.
    
    * fsl_imx25_realize(), fsl_imx31_realize(), dp8393x_realize()
    
      DeviceClass.realize() methods, errors handled sanely further up the
      call chain.
    
    We're good.  Test case again behaves:
    
        $ qemu-system-x86_64 -m 10000000
        qemu-system-x86_64: cannot set up guest memory 'pc.ram': Cannot allocate memory
        [Exit 1 ]
    
    The next commits will repair the rest of commit ef701d7's damage.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Message-Id: <1441983105-26376-3-git-send-email-armbru@redhat.com>
    Reviewed-by: Peter Crosthwaite <crosthwaite.peter@gmail.com>

--- hw/arm/armv7m.c
+++ hw/arm/armv7m.c
@@ -253,7 +253,7 @@ qemu_irq *armv7m_translated_init(MemoryRegion *system_memory,
     /* Hack to map an additional page of ram at the top of the address
        space.  This stops qemu complaining about executing code outside RAM
        when returning from an exception.  */
-    memory_region_init_ram(hack, NULL, "armv7m.hack", 0x1000, &error_abort);
+    memory_region_init_ram(hack, NULL, "armv7m.hack", 0x1000, &error_fatal);
     vmstate_register_ram_global(hack);
     memory_region_add_subregion(system_memory, 0xfffff000, hack);
 
